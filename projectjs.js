6f900d6a42668b3b695015583b98ea367e23e40f
onskallel <onskallel05@gmail.com>
2024-09-20 17:36:50 +0100

    first commit

---
+++ projectjs.js
@@ -0,0 +1,258 @@
+var board;
+var score = 0;
+var rows = 4;
+var columns = 4;
+
+window.onload = function() {
+    setGame();
+    loadHighScore(); // Charger le score élevé au démarrage
+    document.getElementById("reset-button").addEventListener("click", resetGame);
+}
+function loadHighScore() {
+    highScore = localStorage.getItem("highScore") ? parseInt(localStorage.getItem("highScore")) : 0;
+    document.getElementById("high-score").innerText = highScore; // Display the high score
+}
+
+// Update the high score if the current score exceeds it
+function updateHighScore() {
+    if (score > highScore) {
+        highScore = score;
+        localStorage.setItem("highScore", highScore); // Save new high score to local storage
+        document.getElementById("high-score").innerText = highScore; // Update the high score display
+    }
+}
+function slide(row) {
+    row = filterZero(row);
+    for (let i = 0; i < row.length - 1; i++) {
+        if (row[i] == row[i + 1]) {
+            row[i] *= 2;
+            row[i + 1] = 0;
+            score += row[i];
+        }
+    }
+    row = filterZero(row);
+    while (row.length < columns) {
+        row.push(0);
+    }
+    score += row[i];
+    // Vérifiez et mettez à jour le score élevé
+    if (score > highScore) {
+        highScore = score;
+        localStorage.setItem("highScore", highScore); // Enregistrez le nouveau score élevé
+        document.getElementById("high-score").innerText = highScore; // Mettez à jour l'affichage
+    }
+    return row;
+}
+
+
+function setGame() {
+    board = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ];
+
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            let num = board[r][c];
+            updateTile(tile, num);
+            document.getElementById("board").append(tile);
+        }
+    }
+    setTwo();
+    setTwo();
+}
+
+function updateTile(tile, num) {
+    tile.innerText = "";
+    tile.classList.value = ""; // clear the classList
+    tile.classList.add("tile");
+    if (num > 0) {
+        tile.innerText = num.toString();
+        if (num <= 4096) {
+            tile.classList.add("x" + num.toString());
+        } else {
+            tile.classList.add("x8192");
+        }
+    }
+}
+
+document.addEventListener('keyup', (e) => {
+    if (e.code == "ArrowLeft") {
+        slideLeft();
+    }
+    else if (e.code == "ArrowRight") {
+        slideRight();
+    }
+    else if (e.code == "ArrowUp") {
+        slideUp();
+    }
+    else if (e.code == "ArrowDown") {
+        slideDown();
+    }
+
+    if (noMovesAvailable()) {
+        document.getElementById("game-over-message").classList.remove("hidden");
+    } else {
+        document.getElementById("score").innerText = score;
+        setTwo();
+    }
+});
+
+function filterZero(row) {
+    return row.filter(num => num != 0);
+}
+
+function slide(row) {
+    row = filterZero(row);
+    for (let i = 0; i < row.length - 1; i++) {
+        if (row[i] == row[i + 1]) {
+            row[i] *= 2;
+            row[i + 1] = 0;
+            score += row[i];
+        }
+    }
+    row = filterZero(row);
+    while (row.length < columns) {
+        row.push(0);
+    }
+    return row;
+}
+
+function slideLeft() {
+    for (let r = 0; r < rows; r++) {
+        let row = board[r];
+        row = slide(row);
+        board[r] = row;
+        for (let c = 0; c < columns; c++) {
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num);
+        }
+    }
+}
+
+function slideRight() {
+    for (let r = 0; r < rows; r++) {
+        let row = board[r];
+        row.reverse();
+        row = slide(row);
+        board[r] = row.reverse();
+        for (let c = 0; c < columns; c++) {
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num);
+        }
+    }
+}
+
+function slideUp() {
+    for (let c = 0; c < columns; c++) {
+        let row = [board[0][c], board[1][c], board[2][c], board[3][c]];
+        row = slide(row);
+        for (let r = 0; r < rows; r++) {
+            board[r][c] = row[r];
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num);
+        }
+    }
+}
+
+function slideDown() {
+    for (let c = 0; c < columns; c++) {
+        let row = [board[0][c], board[1][c], board[2][c], board[3][c]];
+        row.reverse();
+        row = slide(row);
+        row.reverse();
+        for (let r = 0; r < rows; r++) {
+            board[r][c] = row[r];
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num);
+        }
+    }
+}
+
+function setTwo() {
+    if (!hasEmptyTile()) {
+        if (noMovesAvailable()) {
+            document.getElementById("game-over-message").classList.remove("hidden");
+            return;
+        }
+    }
+
+    let found = false;
+    while (!found) {
+        let r = Math.floor(Math.random() * rows);
+        let c = Math.floor(Math.random() * columns);
+        if (board[r][c] == 0) {
+            board[r][c] = 2;
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            tile.innerText = "2";
+            tile.classList.add("x2");
+            found = true;
+        }
+    }
+}
+
+function hasEmptyTile() {
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            if (board[r][c] == 0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function noMovesAvailable() {
+    if (hasEmptyTile()) return false;
+    
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns - 1; c++) {
+            if (board[r][c] == board[r][c + 1]) {
+                return false;
+            }
+        }
+    }
+    
+    // for (let c = 0; c < columns; c++) {
+    //     for (let r = 0; r < rows - 1; r++) {
+    //         if (board[r][c] == board[r + 1][c]) {
+    //             return false;
+    //         }
+    //     }
+    // }
+    
+    console.log("aaaa")
+    return true;
+}
+
+function resetGame() {
+    console.log("resetGaame");
+    score = 0;
+    document.getElementById("score").innerText = score;
+
+    board = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ];
+
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            updateTile(tile, board[r][c]);
+        }
+    }
+
+    document.getElementById("game-over-message").classList.add("hidden");
+    setTwo();
+    setTwo();
+}
